747526ba1dd982974fea526446b133ff42e6ec42
ICanPlayAndMakeGames <119879085+ICanPlayAndMakeGames@users.noreply.github.com>
2023-02-10 20:07:01 +0000

    Update testing-event.js

--- functions/testing-event.js
+++ functions/testing-event.js
@@ -2,8 +2,9 @@ const https = require('https');
 const noblox = require('noblox.js')
 
 exports.handler = async function(event, context, callback) {
-  console.log("Hello there")
+  
   return {
+    console.log("Hello there")
     statusCode:200,
     body:JSON.stringify({message:"Hello guys!"})
    }
